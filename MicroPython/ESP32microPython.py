030adc851ae4fb4ae91c4fd4becbf291dfc5706c
Said <bedrich.said@honeywell.com>
2019-02-19 13:45:16 +0100

    Updated MicroPython manual according to the ALKS.

--- MicroPython/ESP32microPython.py
+++ MicroPython/ESP32microPython.py
@@ -6,7 +6,78 @@
 # Příkaz help() vám zobrazí nápovědu v angličtině, doporučuji přečíst
 help()
 
-# Pokud chceme pracovat se vstupy a výstupy (GPIO), musíme nejprve importovat modul machine
+# Pro práci s periferiemi ALKS je nutné procesoru říct, že je připojen k ALKS a importovat vhodnou knihovnu s definicemi jednotlivých příkazů použitých níže
+import alks
+
+# Nejprve je třeba nastavit všechny piny (nožičky) procesoru na vstupní nebo výstupní nebo k nim přiřadit správnou periferii podle toho, k čemu jsou připojené. V případě ALKS to můžeme udělat zavoláním funkce:
+alks.setupAll()
+
+# Nyní můžeme otestovat, jestli je všechno správně a LEDky blikají:
+alks.testLeds()
+
+# Můžeme přidat interval blikání v milisekundách jako parametr funkce testLeds():
+alks.testLeds(500)
+
+# Rozsvěcování a zhasínání jednotlivých LED provádíme pomocí funkcí "on()" a "off()", případně "value(1)" odpovídá "on()" a "value(0)" odpovídá "off()". Jednotlivé LED jsou reprezentovány počátečními písmeny svých barev:
+alks.r.on()
+alks.r.off()
+alks.g.on()
+alks.g.off()
+alks.b.on()
+alks.b.off()
+alks.y.on()
+alks.y.off()
+alks.RGBr.on()
+alks.RGBg.on()
+alks.RGBb.on()
+alks.g.value(1)
+
+# Poznámka: Pokud místo "import alks" napíšeme:
+from alks import *
+# tak nebudeme muset při každém zavolání libovolné funkce z knihovny ALKS psát "alks.jméno_funkce()":
+setupAll()
+
+# Pokud chceme používat jen některé periferie, nemusíme volat funkci "setupAll()", ale jen její části:
+setupLeds()
+setupRgbLeds()
+setupButtons()
+setupPiezo()
+
+# Za chvíli budeme potřebovat funkci, která jen čeká nějaký zadaný čas v milisekundách:
+alks.delay(1000)
+
+# Pokud chceme, aby se příkazy vykonávaly pořád dokola, využijeme cyklus, který jsme se učili dříve:
+# Terminál vás v tomto případě nechá vypsat více řádků. Pro ukončení je třeba na posledním řádku stisknout backspace a potom enter.
+while True:
+	r.on()
+	delay(500)
+	r.off()
+	delay(500)
+
+# Příklad: Naprogramuje semafor pro auta.
+
+# Míchání barev pomocí RGB LED:
+# Opět nejprve musíme procesoru říct, na kterých pinech je RGB LED připojená a že umí měnit svůj jas:
+alks.setupRgbPwm()
+
+# Barvy nastavujeme mícháním ze tří základních barev (červená - Red, zelená - Green, modrá - Blue). Například bílou barvu získáme takto smícháním všech tří zákaldních barev dohromady:
+alks.RGBr.duty(1023)
+alks.RGBg.duty(1023)
+alks.RGBb.duty(1023)
+
+# Všiměte si, že číslo 1023 nastavuje jas dané barvy a může nabývat hodnot od 0 (nesvítí vůbec) do 1023 (svítí naplno).
+
+# Příklad: Jakou barvu namícháme pomocí těchto příkazů?
+alks.RGBr.duty(512)
+alks.RGBg.duty(256)
+alks.RGBb.duty(128)
+
+# Příklad: Vytvořte program, který zajistí postupné stmívání a rozsvěcování nějaké barvy na RGB LED.
+
+
+## Pokročilé:
+
+# Pokud chceme pracovat se vstupy a výstupy (GPIO) obecně, musíme nejprve importovat modul machine
 import machine
 
 # Pokud chceme pracovat s WiFi, potřebujeme modul metwork
